7d5f14add39de6d14702dde7bb760fe32b4ce183
André Dias <diaso.andre@outlook.com>
2025-10-22 16:49:44 +0000

    tests: add custom environment with dotenv

--- api/tests/index.ts
+++ api/tests/index.ts
@@ -1,5 +1,10 @@
 import { treaty } from "@elysiajs/eden";
 import { App } from "@/http/app";
-const api_spec = treaty(App);
+import * as dotenv from "dotenv";
 
+const envFile =
+	process.env.NODE_ENV === "production" ? "../.env.production" : "../.env.development";
+dotenv.config({ path: envFile, override: true });
+
+const api_spec = treaty(App);
 export { api_spec };
