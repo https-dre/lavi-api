128d9de3aed8eabd18bf323154430fb789ae21f9
https-dre <diaso.andre@outlook.com>
2025-11-10 19:02:41 -0300

    fix: add new path with 'infra' on schema location

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -11,7 +11,7 @@ dotenv.config({ path: envFile });
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/database/tables.ts",
+  schema: "./src/infra/database/tables.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
