49158b665e053a7f69e4ff016a337b5070911c9d
https-dre <diaso.andre@outlook.com>
2025-11-20 18:26:48 -0300

    fix: remove default import and use '* as t'

--- api/src/modules/customer/customer-repository.ts
+++ api/src/modules/customer/customer-repository.ts
@@ -1,14 +1,14 @@
 import { randomUUID } from "crypto";
 import { ICustomerRepository } from "@/types/repositories";
 import { db } from "@/infra/database/conn";
-import tables from "@/infra/database/tables";
+import * as t from "@/infra/database/tables";
 import { CustomerModel } from "@/types/models";
 import { eq } from "drizzle-orm";
 
 export class CustomerRepository implements ICustomerRepository {
   public async save(data: Omit<CustomerModel, "id">): Promise<CustomerModel> {
     const result = await db
-      .insert(tables.customer)
+      .insert(t.customer)
       .values({
         id: randomUUID(),
         ...data,
@@ -21,16 +21,16 @@ export class CustomerRepository implements ICustomerRepository {
   public async findByDoc(doc: string): Promise<CustomerModel> {
     const result: CustomerModel[] = await db
       .select()
-      .from(tables.customer)
-      .where(eq(tables.customer.doc_blind_index, doc));
+      .from(t.customer)
+      .where(eq(t.customer.doc_blind_index, doc));
     return result[0];
   }
 
   public async findByEmail(email: string): Promise<CustomerModel> {
     const result: CustomerModel[] = await db
       .select()
-      .from(tables.customer)
-      .where(eq(tables.customer.email_blind_index, email));
+      .from(t.customer)
+      .where(eq(t.customer.email_blind_index, email));
 
     return result[0];
   }
@@ -38,14 +38,14 @@ export class CustomerRepository implements ICustomerRepository {
   public async findById(id: string): Promise<CustomerModel> {
     const result: CustomerModel[] = await db
       .select()
-      .from(tables.customer)
-      .where(eq(tables.customer.id, id));
+      .from(t.customer)
+      .where(eq(t.customer.id, id));
 
     return result[0];
   }
 
   public async delete(id: string): Promise<void> {
-    await db.delete(tables.customer).where(eq(tables.customer.id, id));
+    await db.delete(t.customer).where(eq(t.customer.id, id));
   }
 
   public async update(
@@ -53,15 +53,15 @@ export class CustomerRepository implements ICustomerRepository {
     id: string,
   ): Promise<void> {
     await db
-      .update(tables.customer)
+      .update(t.customer)
       .set(updates)
-      .where(eq(tables.customer.id, id));
+      .where(eq(t.customer.id, id));
   }
 
   public async listAllIds(): Promise<{ id: string }[]> {
     const result = await db
-      .select({ id: tables.customer.id })
-      .from(tables.customer);
+      .select({ id: t.customer.id })
+      .from(t.customer);
     return result;
   }
 }
